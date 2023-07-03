3b022c516b8e76846a0c7e62a83a4c04ac177dca
Zacharooos <gabrielcesarfernandescarvalho@gmail.com>
2023-07-03 20:19:04 -0300

    Criação da Base do Código
    Em breve adiciono a conexão base do Discord.

---
+++ src/fileExemplo.py
@@ -0,0 +1,6 @@
+# Arquivo de exemplo vs 0.1
+# Escreva aqui um breve propósito desse arquivo, que tipo de funções devem ser inseridas aqui.
+
+def funcaoExemplo():
+    print("Hello Cefet!")
+
---
+++ src/main.py
@@ -0,0 +1,12 @@
+# CeffyBot version 0.1
+# Corpo principal da aplicação, funções não devem ser declaradas neste arquivo!
+
+# Notas importantes:
+# --- Matheus se você escrever dódigo igual você faz no estágio eu vou te cortar🔪.
+
+# Imports de files auxiliares
+from fileExemplo import *
+
+# Chamadas da funções
+if __name__ ==  "__main__":
+    funcaoExemplo()
