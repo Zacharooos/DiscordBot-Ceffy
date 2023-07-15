0673eccd39ac8fed6f10664577e080760f2eca51
Zacharooos <gabrielcesarfernandescarvalho@gmail.com>
2023-07-14 21:10:34 -0300

    Remoção de FileExemplo
    Remoção de FileExemplo tendo em vista que o projeto já tem corpo o suficiente para se referencias por funções reais já preparadas.

--- src/fileExemplo.py
+++
@@ -1,7 +0,0 @@
-# Arquivo de exemplo vs 0.2
-# Escreva aqui um breve propósito desse arquivo, que tipo de funções devem ser inseridas aqui.
-
-def funcaoExemplo():
-    print("Hello Cefet!")
-
-    # Escreva from 'fileConfigurar import *' na Main para ter acesso as funções aqui presentes.
