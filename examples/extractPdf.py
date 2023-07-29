48496f1ff5a3b6baf64329621e145f3a8d877c6a
CarlosLagreca <carloslagrecajr02@gmail.com>
2023-07-29 17:13:50 -0300

    Ajustes na tradução de pdf

--- examples/extractPdf.py
+++ examples/extractPdf.py
@@ -1,11 +1,44 @@
+# Funções responsáveis por extrair texto de um pdf.
+# -> Esse código aq ta gambiarra atrás de gambiarra, mas funciona.
+# -> Não temos nenhuma outra ideia de como fazer isso aq... Já foi uma merda fazer esse funcionar da forma q ta
+# -> Otimização importante para o futuro.
+
 from pdfminer.high_level import extract_text
-from os import listdir
+
+
+# "processa" a string para aplicar o backspace
+def apply_backspace_effect(input_str):
+    result = []
+    for char in input_str:
+        if char == '\b':
+            if result:
+                result.pop()
+        else:
+            result.append(char)
+    return ''.join(result)
+
+
+# "processa" a string para aplica delete (x7f)
+def apply_delete_effect(input_str):
+    result = []
+    i = 0
+    while i < len(input_str):
+        char = input_str[i]
+        if char == '\x7f':
+            if i + 1 < len(input_str):
+                i += 1
+        else:
+            result.append(char)
+        i += 1
+    return ''.join(result)
+
 
 # Função que recebe uma string e formata ela manualmente
 def format_text(text) -> str:
     
     # Cedilha
     text = text.replace("¸c", "ç")
+    text = text.replace("C¸", "Ç\x7f")
 
     # Acentos agudos
     text = text.replace("´a", "á")
@@ -14,10 +47,10 @@ def format_text(text) -> str:
     text = text.replace("´o", "ó")
     text = text.replace("´u", "ú")
 
-    text = text.replace("´A", "Á")
+    text = text.replace("´A", "\bÁ")
     text = text.replace("´E", "É")
     text = text.replace("´ı", "Í")
-    text = text.replace("´O", "Ó")
+    text = text.replace("´O", "\bÓ")
     text = text.replace("´U", "Ú")
 
     
@@ -29,13 +62,20 @@ def format_text(text) -> str:
     text = text.replace("˜O", "Õ")
 
     # Circunflexo
+    text = text.replace("ˆa", "â")
     text = text.replace("ˆe", "ê")
     text = text.replace("ˆo", "ô")
 
+    text = text.replace("ˆA", "Â")
     text = text.replace("ˆE", "Ê")
     text = text.replace("ˆO", "Ô")
 
+    # Aplicando caracteres backslash
+    text = apply_backspace_effect(text)
+    text = apply_delete_effect(text)
+
     return text
+
 
 def read_pdf_pdfminer(fileObj) -> None:
 
@@ -44,16 +84,20 @@ def read_pdf_pdfminer(fileObj) -> None:
 
     formatted_text = format_text(text)
 
-    print(formatted_text)
+    return formatted_text
+
+
+if __name__ == "__main__":
+
+    pdfFileObj = open('examples/ementaExemplo.pdf', 'rb')
+
+    pdf_string = read_pdf_pdfminer(pdfFileObj)
+
+    print(pdf_string)
 
     f = open("teste.txt", "w", encoding='utf-8')
-    f.write(formatted_text)
+    f.write(pdf_string)
     f.close()
 
-
-pdfFileObj = open('ementaExemplo.pdf', 'rb')
-
-read_pdf_pdfminer(pdfFileObj)
-
-pdfFileObj.close()
+    pdfFileObj.close()
 
